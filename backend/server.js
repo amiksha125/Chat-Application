9eeee3f1a6c520e1d86bbbf403c9708fb6167799
amiksha125 <amikshagajbhiye@gmail.com>
2024-10-11 00:37:46 +0530

    Connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config()
 const app = express()
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+console.log("Connected to mongoDB")
+}).catch((err) => {
+    console.log(err)
+})
 const PORT = process.env.PORT || 3000
 app.get("/",(req,res) => {
     res.send("Hello World")
@@ -11,6 +17,6 @@ app.get("/",(req,res) => {
 // app.listen(3000,() => {
 //     console.log("Server is running on port 3000");
 // })
-app.listen(PORT, () => {
+app.listen(PORT,() => {
     console.log("Server is running on port "+PORT);
 })
