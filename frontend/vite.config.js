5601cd66d37dfd5f39b3d54f585cca0406668d31
amiksha125 <amikshagajbhiye@gmail.com>
2024-12-11 16:19:17 +0530

    Setup proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,14 +6,30 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+
+//     port: 5173, // Or another port if necessary
+//     open: true,
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Or another port if necessary
-    open: true,
+    proxy: {
+       "/api": {
+        target: "http://locahost:3000",
+        secure: false,
+       },
+    },
   },
 });
 
